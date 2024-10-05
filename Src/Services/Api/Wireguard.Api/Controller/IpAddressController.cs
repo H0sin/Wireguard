42c29a279c41efb11268a7ff1d0659b679305eab
hosseinfaraji8079 <h.faraji8079@gmail.com>
2024-10-05 16:24:10 +0330

    change host for connection string

---
+++ Src/Services/Api/Wireguard.Api/Controller/IpAddressController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wireguard.Api.Data.Repositories;
+using Wireguard.Api.Filters;
+
+namespace Wireguard.Api.Controller;
+
+[ApiController]
+[Route("[controller]")]
+[ApiResultFilter]
+[ServiceFilter(typeof(ExceptionHandlerFilter))]
+public class IpAddressController(IIpAddressRepository ipAddressRepository) : ControllerBase
+{
+    
+}
