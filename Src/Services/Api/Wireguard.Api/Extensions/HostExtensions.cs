f7f503a91ddd8c3da95918af3a30318aba6d30d5
hosseinfaraji8079 <h.faraji8079@gmail.com>
2024-10-05 18:34:13 +0330

    fixed migration async

--- Src/Services/Api/Wireguard.Api/Extensions/HostExtensions.cs
+++ Src/Services/Api/Wireguard.Api/Extensions/HostExtensions.cs
@@ -17,68 +17,94 @@ namespace Wireguard.Api.Extensions
                 // migrate database
                 try
                 {
-                    logger.LogInformation("migrating posgtresql database");
+                    logger.LogInformation("Migrating PostgreSQL database...");
 
                     using var connection =
                         new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
-                    
+
                     connection.Open();
-                    
+
                     using var command = new NpgsqlCommand
                     {
                         Connection = connection
                     };
 
-                                        
-                    command.CommandText = "DROP TABLE IF EXISTS IpAddress CASCADE";
-                    command.ExecuteNonQuery();
-                    
-                    command.CommandText = "DROP TABLE IF EXISTS Interface CASCADE";
-                    command.ExecuteNonQuery();
+                    // Check if 'Interface' table exists
+                    command.CommandText = "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'interface')";
+                    var interfaceExists = (bool)command.ExecuteScalar();
 
-                    command.CommandText = """
-                                          CREATE TABLE Interface(
-                                              Id BIGSERIAL PRIMARY KEY,
-                                              Name VARCHAR(60) NOT NULL,
-                                              Address VARCHAR(100) NOT NULL,
-                                              EndPoint VARCHAR(200) NOT NULL,
-                                              SaveConfig BOOLEAN NOT NULL,
-                                              PreUp VARCHAR(200) DEFAULT '',
-                                              PostUp VARCHAR(200) DEFAULT '',
-                                              PreDown VARCHAR(200) DEFAULT '',
-                                              PostDown VARCHAR(200) DEFAULT '',
-                                              ListenPort VARCHAR(50) DEFAULT '',
-                                              PrivateKey VARCHAR(255),
-                                              IpAddress VARCHAR(100),
-                                              PublicKey VARCHAR(255),
-                                              CreateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                              UpdateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                              Status VARCHAR(30)
-                                          );
-                                          """;
+                    if (!interfaceExists)
+                    {
+                        // Create 'Interface' table if it doesn't exist
+                        command.CommandText = """
+                                              CREATE TABLE Interface(
+                                                  Id BIGSERIAL PRIMARY KEY,
+                                                  Name VARCHAR(60) NOT NULL,
+                                                  Address VARCHAR(100) NOT NULL,
+                                                  EndPoint VARCHAR(200) NOT NULL,
+                                                  SaveConfig BOOLEAN NOT NULL,
+                                                  PreUp VARCHAR(200) DEFAULT '',
+                                                  PostUp VARCHAR(200) DEFAULT '',
+                                                  PreDown VARCHAR(200) DEFAULT '',
+                                                  PostDown VARCHAR(200) DEFAULT '',
+                                                  ListenPort VARCHAR(50) DEFAULT '',
+                                                  PrivateKey VARCHAR(255),
+                                                  IpAddress VARCHAR(100),
+                                                  PublicKey VARCHAR(255),
+                                                  CreateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                                  UpdateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                                  Status VARCHAR(30)
+                                              );
+                                              """;
+                        command.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        // Modify 'Interface' table if it exists
+                        command.CommandText = """
+                                              ALTER TABLE Interface
+                                              ADD COLUMN IF NOT EXISTS NewColumn VARCHAR(100) DEFAULT 'NewValue'
+                                              """;
+                        command.ExecuteNonQuery();
+                    }
 
-                    command.ExecuteNonQuery();
+                    // Check if 'IpAddress' table exists
+                    command.CommandText = "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'ipaddress')";
+                    var ipAddressExists = (bool)command.ExecuteScalar();
 
-                    command.CommandText = """
-                                          CREATE TABLE IpAddress(
-                                              Id BIGSERIAL PRIMARY KEY,
-                                              InterfaceId BIGINT,
-                                              Ip VARCHAR(50) NOT NULL,
-                                              Available BOOLEAN DEFAULT FALSE,
-                                              CreateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                              UpdateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                              FOREIGN KEY (InterfaceId) REFERENCES Interface(Id)
-                                          )
-                                          """;
-                    command.ExecuteNonQuery();
+                    if (!ipAddressExists)
+                    {
+                        // Create 'IpAddress' table if it doesn't exist
+                        command.CommandText = """
+                                              CREATE TABLE IpAddress(
+                                                  Id BIGSERIAL PRIMARY KEY,
+                                                  InterfaceId BIGINT,
+                                                  Ip VARCHAR(50) NOT NULL,
+                                                  Available BOOLEAN DEFAULT FALSE,
+                                                  CreateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                                  UpdateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                                  FOREIGN KEY (InterfaceId) REFERENCES Interface(Id)
+                                              );
+                                              """;
+                        command.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        // Modify 'IpAddress' table if it exists
+                        command.CommandText = """
+                                              ALTER TABLE IpAddress
+                                              ADD COLUMN IF NOT EXISTS SubnetMask VARCHAR(50)
+                                              """;
+                        command.ExecuteNonQuery();
+                    }
 
-                    // seed data
+                    // Seed data if necessary
 
-                    logger.LogInformation("migration has been completed!!!");
+                    logger.LogInformation("Migration has been completed!");
                 }
                 catch (NpgsqlException ex)
                 {
-                    logger.LogError("an error has been occured");
+                    logger.LogError("An error occurred while migrating the PostgreSQL database", ex);
 
                     if (retryForAvailability < 50)
                     {
