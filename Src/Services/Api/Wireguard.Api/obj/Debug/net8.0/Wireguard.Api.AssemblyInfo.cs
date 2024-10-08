0fe5a9a653ad5ff39c25a8696d00ca6a30b3dd09
hosseinfaraji8079 <h.faraji8079@gmail.com>
2024-10-08 19:50:27 +0330

    fixed nuget

--- Src/Services/Api/Wireguard.Api/obj/Debug/net8.0/Wireguard.Api.AssemblyInfo.cs
+++ Src/Services/Api/Wireguard.Api/obj/Debug/net8.0/Wireguard.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wireguard.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2df849d04eb0f15f3cf782e9c8ba5c7fec97872a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5e110509701784314531de0d6c3cbd7c570972")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wireguard.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wireguard.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
