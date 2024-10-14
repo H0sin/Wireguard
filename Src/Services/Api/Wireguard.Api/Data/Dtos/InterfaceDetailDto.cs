46bd7cbe3937752926552bac021cc6043ad4aab8
hosseinfaraji8079 <h.faraji8079@gmail.com>
2024-10-14 16:23:20 +0330

    ListenPort

--- Src/Services/Api/Wireguard.Api/Data/Dtos/InterfaceDetailDto.cs
+++ Src/Services/Api/Wireguard.Api/Data/Dtos/InterfaceDetailDto.cs
@@ -3,8 +3,8 @@
 public class InterfaceDetailDto
 {
     public string Name { get; set; }
-    public string Type { get; set; }
-    public string Port { get; set; }
+    public string? Type { get; set; }
+    public string ListenPort { get; set; }
     public string PublicKey { get; set; }
     public long TotalData { get; set; }
     public long TotoalDataUsed { get; set; }
