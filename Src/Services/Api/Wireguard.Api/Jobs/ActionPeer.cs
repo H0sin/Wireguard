dcec94fe8a059d84c45546dac001f1da75f4dbb1
hosseinfaraji8079 <h.faraji8079@gmail.com>
2024-10-16 21:43:43 +0330

    FixedExpireTime

--- Src/Services/Api/Wireguard.Api/Jobs/ActionPeer.cs
+++ Src/Services/Api/Wireguard.Api/Jobs/ActionPeer.cs
@@ -71,7 +71,7 @@ public class ActionPeer : IJob
                         "UPDATE Peer SET StartTime = @StartTime, ExpireTime = @ExpireTime WHERE PublicKey = @PublicKey",
                         new
                         {
-                            ExpireTime = currentEpochTime + peer.OnHoldExpireDurection,
+                            ExpireTime = (currentEpochTime + (peer.OnHoldExpireDurection * 1000)),
                             PublicKey = peer.PublicKey,
                             StartTime = currentEpochTime
                         });
