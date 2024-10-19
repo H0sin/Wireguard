e3ef3cbf307bbdb2aba3d24e5848c368f4b2fbd4
hosseinfaraji8079 <h.faraji8079@gmail.com>
2024-10-20 02:27:29 +0330

    rabbit mq

--- Src/Services/Api/Wireguard.Api/obj/Debug/net8.0/Wireguard.Api.AssemblyInfo.cs
+++ Src/Services/Api/Wireguard.Api/obj/Debug/net8.0/Wireguard.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wireguard.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdcd3b35a088e422eeb8a2a0867cc9b5d25d4f0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e070570dbf40cba0098b29edadfedeeadc9d0717")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wireguard.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wireguard.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
